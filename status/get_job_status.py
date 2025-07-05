aa16a8e5c2eaa25d696d33055f5ba7462e78c3f1
b3loreto <b3loreto@gmail.com>
2025-07-05 08:31:47 -0700

    add feature get-job-status

--- status/get_job_status.py
+++ status/get_job_status.py
@@ -1,1 +1,15 @@
+import redis
+import sys
 
+r = redis.Redis(host='localhost',port='6379', db='0')
+
+def get_status(job_id):
+    status = r.hget("job_status", job_id)
+    if status:
+        print(f"🧾 Job {job_id} status: {status.decode('utf-8')}")
+    else:
+        print("⚠️ Job not found.")
+
+if __name__ == "__main__":
+    job_id = sys.argv[1]
+    get_status(job_id)
