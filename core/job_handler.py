438c1d892e7e544d6578948948b00b21e6c22cb4
b3loreto <b3loreto@gmail.com>
2025-07-06 12:16:59 -0700

    added type hints

--- core/job_handler.py
+++ core/job_handler.py
@@ -1,19 +1,30 @@
 import time
+from typing import Dict, List, Any
 
-def handle_print_message(payload):
+def handle_print_message(payload: Dict[str,str]):
     print(f"📝 {payload['message']}")
     time.sleep(1)
 
-def handle_add(payload):
-    result = payload["a"] + payload["b"]
-    print(f"➕ {payload['a']} + {payload['b']} = {result}")
+def handle_add(payload: Dict[str,int]):
+    result = 0
+    values = []
+    for key, val in payload.items():
+        result += val
+        values.append(str(val))
+
+    if len(values) == 1:
+        values.append("0")
+    calculation = " + ".join(values)
+    print(f"➕ {calculation} = {result}")
     time.sleep(1)
 
-def handle_reverse_string(payload):
-    print(payload["text"][::-1])
+def handle_reverse_string(payload: Dict[str,str]):
+    text = payload["text"]
+    reversed_text = (payload["text"][::-1])
+    print(f"🔁 {text} → {reversed_text}")
     time.sleep(1)
 
-def get_handler(job_type):
+def get_handler(job_type: str):
     handler = {
         "print_message": handle_print_message,
         "add": handle_add,
